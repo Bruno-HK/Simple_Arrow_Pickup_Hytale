647f27d2e367008a0bb38dd8dcbcaf62e6bd8fd6
BrunoBolas <brunobolas96@hotmail.com>
2026-01-29 14:15:48 +0000

    shit

---
+++ src/main/java/com/hytsustudio/ArrowPickupPlugin.java
@@ -0,0 +1,19 @@
+package com.hytsustudio;
+
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
+
+import javax.annotation.Nonnull;
+
+public class ArrowPickupPlugin extends JavaPlugin {
+
+    public ArrowPickupPlugin(@Nonnull JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    protected void setup() {
+        EntityStore.REGISTRY.registerSystem(new ArrowPickupSystem());
+    }
+}
